1897485c87008731e4c0f27fbb3d7d0ce9b5312c
chabiri <chyb627@naver.com>
2021-07-28 16:59:13 +0900

    portfolio init 입니다

---
+++ index.php
@@ -0,0 +1,471 @@
+<!DOCTYPE HTML>
+<!--
+	Paradigm Shift by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Paradigm Shift by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section class="intro">
+						<header>
+							<h1>개발하는 차영빈</h1>
+							<p><h3>포토폴리오</h3><a href="https://www.instagram.com/chabiri">@chabiri</a> / <a href="https://github.com/chyb627">my git</a></p>
+							<ul class="actions">
+								<li><a href="#first" class="arrow scrolly"><span class="label">Next</span></a></li>
+							</ul>
+						</header>
+						<div class="content">
+							<span class="image fill" data-position="center"><img src="images/profile.JPG" alt="" /></span>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section id="first">
+						<header>
+							<h2>profile</h2>
+							<img src="images/profile2.JPG" alt="" /></span>
+						</header>
+						<div class="content">
+							<p><strong>학력</strong><br> 강원대학교 산업경영공학과 졸업 (2011.03~2019.08)</p>
+							<p><strong>경력사항</strong><br>
+							마인드원 사원 SCADA/HMI (2020.12~2021.06)<br>
+							우리젠 인턴 Flutter, JavaScript 개발 (2020.08~2020.12)
+							</p>
+							<p><strong>교육사항</strong><br>
+							싸이몬 CIMON SCADA 기본 (총 12시간)<br>
+							한국BEMS 에너지관리시스템(EMS) (총 396시간)
+							</p>
+							<p><strong>수상경력</strong><br>
+							통신 주특기대회 우승 대대장 표창장 (2012.08)<br>
+							EMS 교육과정 모범상 (2020.07)
+							</p>
+							<p><strong>자격사항</strong><br>
+							정보처리기사 필기 (2021.03)<br>
+							한국사능력검정시험 2급 (2016.01)
+							</p>
+							<p><strong>어학사항</strong><br>
+							TOEIC 695점 (2019.12)
+							</p>
+							<span class="image main"><img src="images/profile3.jpg" alt="" /></span>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>What do I like</h2>
+						</header>
+						<div class="content">
+							<p><strong>#차영빈</strong> &nbsp;  <strong>#취미생활</strong></p>
+							<ul class="feature-icons">
+								<li class="icon solid fa-laptop-code">Coding</li>
+								<li class="icon solid fa-dumbbell">Work out</li>
+								<li class="icon solid fa-bicycle">Bicycle</li>
+								<li class="icon solid fa-gamepad">Game</li>
+								<li class="icon solid fa-hiking">Hiking</li>
+								<li class="icon solid fa-video">Movie</li>
+							</ul>
+							<p><strong>동호회 활동</strong><br>
+							"샤방 양재천 라이딩 (2020.5~현재)" <br>
+							"일원동 헬스 모임 (2019.12~현재)"
+							</p>
+						</div>11
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>Why Programmer?</h2>
+						</header>
+						<div class="content">
+							<p>EMS 교육 과정을 통해 프로그래밍에 관심이 생겼습니다. 
+								교육에서 C언어와 파이썬, R언어를 공부하였고, 
+								인턴을 통해 Flutter를 이용한 소프트웨어 개발과 
+								자바 스크립트를 이용한 로직 구성을 하면서, 코딩에 관심을 갖게 되었고,
+								코딩을 통해 결과물을 만들면서 재미를 느꼈습니다.
+								현업을 통하여 더욱 발전하고 지속적인 공부로 멋진 개발자가 되겠습니다.</p>
+						</div>
+					</section>
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>project</h2>
+							
+						</header>
+						<div class="content">
+
+							<p>
+							<strong>우리젠 개발부 인턴 근무 (2020.08 ~2020.12)</strong><br>
+							<strong>프로젝트명: </strong>자동제어 소프트웨어 개발<br> 
+							<strong>사용기술: </strong>Flutter<br> 
+							<strong>프로젝트소개: </strong>빌딩 내 기기 (펌프, 보일러, 공조기 등) 자동제어 프로그램 개발<br> 
+							<strong>담당업무: </strong>Flutter를 활용하여 메뉴 창 구성, 스케줄 및 알람 페이지의 Flont 부분 코딩 (위젯, 라이브러리 사용)<br>
+							<span class="image main"><img src="images/project1.png" alt="" /></span>
+							</p>
+							
+							<p>
+								<strong>우리젠 개발부 인턴 근무 (2020.08 ~2020.12)</strong><br>
+								<strong>프로젝트명: </strong>퇴계원 종합행정타운 자동제어 스크립트 작성<br> 
+								<strong>사용기술: </strong>JavaScript<br> 
+								<strong>프로젝트소개: </strong>빌딩 내 기기 (펌프, 보일러, 공조기 등) 자동제어 프로그램 개발<br> 
+								<strong>담당업무: </strong>퇴계원 종합행정타운 빌딩의 장비 (보일러, 저수조, 우수조, FAN 등)
+								들을 맵핑시키고 Getval, Setval를 통해서 자동제어 스크립트(보일러의 펌프교번제어, 우수조의 수위
+								 조절 제어, 일산화탄소 농도에 따른 FAN가동) 작성<br> 
+								 <span class="image main"><img src="images/project2.png" alt="" /></span>
+								 <span class="image main"><img src="images/project3.png" alt="" /></span>
+								 <span class="image main"><img src="images/project4.png" alt="" /></span>
+								</p>
+						</div>
+					</section>
+
+							
+				<!-- Elements -->
+				<!--
+					<section>
+						<header>
+							<h2>Elements</h2>
+						</header>
+						<div class="content">
+
+							<section>
+								<header>
+									<h3>Text</h3>
+								</header>
+								<div class="content">
+									<p>This is <b>bold</b> and this is <strong>strong</strong>. This is <i>italic</i> and this is <em>emphasized</em>.
+									This is <sup>superscript</sup> text and this is <sub>subscript</sub> text.
+									This is <u>underlined</u> and this is code: <code>for (;;) { ... }</code>. Finally, <a href="#">this is a link</a>.</p>
+									<hr />
+									<h2>Heading Level 2</h2>
+									<h3>Heading Level 3</h3>
+									<h4>Heading Level 4</h4>
+									<h5>Heading Level 5</h5>
+									<hr />
+									<h5>Blockquote</h5>
+									<blockquote>Fringilla nisl. Donec accumsan interdum nisi, quis tincidunt felis sagittis eget tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan faucibus. Vestibulum ante ipsum primis in faucibus lorem ipsum dolor sit amet nullam adipiscing eu felis.</blockquote>
+									<h5>Preformatted</h5>
+									<pre><code>i = 0;
+
+while (!deck.isInOrder()) {
+  print 'Iteration ' + i;
+  deck.shuffle();
+  i++;
+}
+
+print 'Sorted in ' + i + ' iterations.';</code></pre>
+								</div>
+							</section>
+
+							<section>
+								<header>
+									<h3>Lists</h3>
+								</header>
+								<div class="content">
+
+									<h4>Unordered</h4>
+									<ul>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Alternate</h4>
+									<ul class="alt">
+										<li>Dolor pulvinar etiam.</li>
+										<li>Sagittis adipiscing.</li>
+										<li>Felis enim feugiat.</li>
+									</ul>
+
+									<h4>Ordered</h4>
+									<ol>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis viverra.</li>
+										<li>Felis enim feugiat.</li>
+										<li>Dolor pulvinar etiam.</li>
+										<li>Etiam vel felis lorem.</li>
+										<li>Felis enim et feugiat.</li>
+									</ol>
+									<h4>Icons</h4>
+									<ul class="icons">
+										<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+									</ul>
+
+									<h4>Actions</h4>
+									<ul class="actions">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions stacked">
+										<li><a href="#" class="button primary">Default</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+								</div>
+							</section>
+
+							<section>
+								<header>
+									<h3>Table</h3>
+								</header>
+								<div class="content">
+									<h4>Default</h4>
+									<div class="table-wrapper">
+										<table>
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+
+									<h4>Alternate</h4>
+									<div class="table-wrapper">
+										<table class="alt">
+											<thead>
+												<tr>
+													<th>Name</th>
+													<th>Description</th>
+													<th>Price</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Item One</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Two</td>
+													<td>Vis ac commodo adipiscing arcu aliquet.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Three</td>
+													<td> Morbi faucibus arcu accumsan lorem.</td>
+													<td>29.99</td>
+												</tr>
+												<tr>
+													<td>Item Four</td>
+													<td>Vitae integer tempus condimentum.</td>
+													<td>19.99</td>
+												</tr>
+												<tr>
+													<td>Item Five</td>
+													<td>Ante turpis integer aliquet porttitor.</td>
+													<td>29.99</td>
+												</tr>
+											</tbody>
+											<tfoot>
+												<tr>
+													<td colspan="2"></td>
+													<td>100.00</td>
+												</tr>
+											</tfoot>
+										</table>
+									</div>
+								</div>
+							</section>
+
+							<section>
+								<header>
+									<h3>Buttons</h3>
+								</header>
+								<div class="content">
+									<ul class="actions">
+										<li><a href="#" class="button primary">Primary</a></li>
+										<li><a href="#" class="button">Default</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button large">Large</a></li>
+										<li><a href="#" class="button">Default</a></li>
+										<li><a href="#" class="button small">Small</a></li>
+									</ul>
+									<ul class="actions">
+										<li><a href="#" class="button primary icon solid fa-download">Icon</a></li>
+										<li><a href="#" class="button icon solid fa-download">Icon</a></li>
+									</ul>
+									<ul class="actions">
+										<li><span class="button primary disabled">Disabled</span></li>
+										<li><span class="button disabled">Disabled</span></li>
+									</ul>
+								</div>
+							</section>
+
+							<section>
+								<header>
+									<h3>Form</h3>
+								</header>
+								<div class="content">
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="demo-name">Name</label>
+												<input type="text" name="demo-name" id="demo-name" value="" placeholder="Jane Doe" />
+											</div>
+											<div class="field half">
+												<label for="demo-email">Email</label>
+												<input type="email" name="demo-email" id="demo-email" value="" placeholder="jane@untitled.tld" />
+											</div>
+											<div class="field">
+												<label for="demo-category">Category</label>
+												<select name="demo-category" id="demo-category">
+													<option value="">-</option>
+													<option value="1">Manufacturing</option>
+													<option value="1">Shipping</option>
+													<option value="1">Administration</option>
+													<option value="1">Human Resources</option>
+												</select>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-low" name="demo-priority" checked>
+												<label for="demo-priority-low">Low</label>
+											</div>
+											<div class="field half">
+												<input type="radio" id="demo-priority-high" name="demo-priority">
+												<label for="demo-priority-high">High</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-copy" name="demo-copy">
+												<label for="demo-copy">Email me a copy</label>
+											</div>
+											<div class="field half">
+												<input type="checkbox" id="demo-human" name="demo-human" checked>
+												<label for="demo-human">Not a robot</label>
+											</div>
+											<div class="field">
+												<label for="demo-message">Message</label>
+												<textarea name="demo-message" id="demo-message" placeholder="Enter your message" rows="6"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Send Message" class="primary" /></li>
+											<li><input type="reset" value="Reset" /></li>
+										</ul>
+									</form>
+								</div>
+							</section>
+
+						</div>
+					</section>
+				-->
+
+				<!-- Section -->
+					<section>
+						<header>
+							<h2>Get in touch</h2>
+						</header>
+						<div class="content">
+							<p><strong>Get in touch with me</strong> </p>
+							<form>
+								<div class="fields">
+									<div class="field half">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" class="button primary" /></li>
+								</ul>
+							</form>
+						</div>
+						<footer>
+							<ul class="items">
+								<li>
+									<h3>Email</h3>
+									<a href="#">chyb627@naver.com</a>
+								</li>
+								<li>
+									<h3>Phone</h3>
+									<a href="#">010-2700-5745</a>
+								</li>
+								<!--<li>
+									<h3>Address</h3>
+									<span>서울시 강남구 양재대로 55길 </span>
+								</li>-->
+								<li>
+									<h3>Elsewhere</h3>
+									<ul class="icons">
+										<!-- <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li> -->
+										<li><a href="https://www.instagram.com/chabiri" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<!--<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>-->
+										<li><a href="https://github.com/chyb627" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+										<!-- <li><a href="#" class="icon brands fa-codepen"><span class="label">Codepen</span></a></li>-->
+									</ul>
+								</li>
+							</ul>
+						</footer>
+					</section>
+
+				<!-- Copyright 
+					<div class="copyright">&copy; Untitled. All rights reserved. Design: <a href="https://html5up.net">HTML5 UP</a>.</div>
+
+			</div>-->
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
